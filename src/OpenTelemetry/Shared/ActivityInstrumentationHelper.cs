bc9d10e8eef3a3116e26955c35a0448d8722bd96
whuanle <1586052146@qq.com>
2024-07-21 16:05:00 +0800

    优化文档

--- src/OpenTelemetry/Shared/ActivityInstrumentationHelper.cs
+++ src/OpenTelemetry/Shared/ActivityInstrumentationHelper.cs
@@ -1,15 +1,20 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
+// <copyright file="ActivityInstrumentationHelper.cs" company="Maomi">
+// Copyright (c) Maomi. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Github link: https://github.com/whuanle/Maomi.MQ
+// </copyright>
 
+#pragma warning disable SA1600 // Elements should be documented
 #nullable disable
 
-#pragma warning disable IDE0005 // Using directive is unnecessary.
 using System;
 using System.Diagnostics;
-#pragma warning restore IDE0005 // Using directive is unnecessary.
 
 namespace OpenTelemetry.Instrumentation;
 
+/// <summary>
+/// Helper.
+/// </summary>
 internal static class ActivityInstrumentationHelper
 {
     internal static readonly Action<Activity, ActivityKind> SetKindProperty = CreateActivityKindSetter();
